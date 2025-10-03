b4c4c8dc1814087b2a1096f8fcde313a5e9b4ef8
Shin-Young Jung <ldsrogan@gmail.com>
2025-10-03 22:54:23 +0900

    feat: added package shared dist

---
+++ packages/shared/dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Gender = exports.RecommendationStatus = exports.ApplicationStatus = exports.LeaderStatus = exports.UserRole = void 0;
+var UserRole;
+(function (UserRole) {
+    UserRole["ADMIN"] = "admin";
+    UserRole["LEADER"] = "leader";
+    UserRole["APPLICANT"] = "applicant";
+})(UserRole || (exports.UserRole = UserRole = {}));
+var LeaderStatus;
+(function (LeaderStatus) {
+    LeaderStatus["PENDING"] = "pending";
+    LeaderStatus["APPROVED"] = "approved";
+})(LeaderStatus || (exports.LeaderStatus = LeaderStatus = {}));
+var ApplicationStatus;
+(function (ApplicationStatus) {
+    ApplicationStatus["DRAFT"] = "draft";
+    ApplicationStatus["AWAITING"] = "awaiting";
+    ApplicationStatus["APPROVED"] = "approved";
+    ApplicationStatus["REJECTED"] = "rejected";
+})(ApplicationStatus || (exports.ApplicationStatus = ApplicationStatus = {}));
+var RecommendationStatus;
+(function (RecommendationStatus) {
+    RecommendationStatus["DRAFT"] = "draft";
+    RecommendationStatus["SUBMITTED"] = "submitted";
+    RecommendationStatus["APPROVED"] = "approved";
+    RecommendationStatus["REJECTED"] = "rejected";
+})(RecommendationStatus || (exports.RecommendationStatus = RecommendationStatus = {}));
+var Gender;
+(function (Gender) {
+    Gender["MALE"] = "male";
+    Gender["FEMALE"] = "female";
+})(Gender || (exports.Gender = Gender = {}));
