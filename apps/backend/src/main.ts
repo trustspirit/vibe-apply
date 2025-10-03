e0bedae7f42d377da714fdfb76996f2a228d5cc5
Shin-Young Jung <ldsrogan@gmail.com>
2025-10-03 15:04:54 +0900

    feat: added binding port

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,9 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://localhost:3000',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -26,7 +30,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const port = process.env.PORT ?? 3001;
-  await app.listen(port);
+  await app.listen(port || 3000, '0.0.0.0');
   console.log(`Backend server is running on http://localhost:${port}`);
 }
 void bootstrap();
